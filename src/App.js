fa5e3303cff78d7638a13d0d8092b481e6c09bbb
AouIssa <spunkyguyyt@gmail.com>
2023-10-08 09:32:50 +0800

    Added profile to the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';
+import Profile from './Profile';  // Import the Profile component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Profile" element={<Profile />} />
+
       </Routes>
     </Router>
   );
